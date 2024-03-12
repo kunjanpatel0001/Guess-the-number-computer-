41ed077f4b9a3c2310a3a5bf919c5b86f9daa079
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-03-12 22:12:52 +0530

    Guess the number (user)
    - randint:- gives error whe low and high are the same

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random       # random package to access randint function
 def guess(x):
     random_number = random.randint(1,x)     # randit function to randomly select a number between a range
     guess = 0
+    
     # looping through the options until we get the right answer hence Loop
     # while loop -  we don't have pre defined universe to iterate over
 
@@ -17,4 +18,23 @@ def guess(x):
             print("Sorry, guess again, Too High.")
 
     print(f"Yay, congrats. You have guessed the number {random_number} correctly. ")
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+     #randint:- gives error whe low and high are the same hence if else condition
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)  
+        else:
+            guess = low     # could also be high bcz low = high   
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct(C)?").lower()        
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+
+    print(f"Yay...the computer guesses your number {guess}, correctly.") 
+# computer_guess(1000)
+    guess(100)
